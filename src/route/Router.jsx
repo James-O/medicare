8661bec2c35d05b3a8f4d5c02808e72c76c3f838
canoooon570 <caulrickchurchill@gmail.com>
2025-08-29 19:35:26 +0100

    added content to FeaturedDepartments.jsx and image

--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homes from '../pages/Homes'
 import AboutUs from '../pages/AboutUs'
+import FeaturedDepartments from '../components/FeaturedDepartments/FeaturedDepartments'
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homes />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path='/FeaturedDepartments' element={<FeaturedDepartments/>} />
       </Routes>
     </BrowserRouter>
   )
