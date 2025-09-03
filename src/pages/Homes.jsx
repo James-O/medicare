24306954c0350d5ebcfbb829fbba9cbf2e663c10
unknown <Bright Uchechukwu>
2025-09-03 12:54:26 +0100

    added more divs

--- src/pages/Homes.jsx
+++ src/pages/Homes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "../components/home/Home"
+import Homess from "../components/home/Homess";
 
 function Homes() {
   return (
     <div>
         <div>
           <Home />
+          <Homess/>
           </div>
     </div>
     
