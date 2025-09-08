985d39b83ec0a9f23de7db91e630ce98964f16aa
unknown <Bright Uchechukwu>
2025-09-08 11:18:15 +0100

    david is making noise

--- src/components/doctor/Doctorh.jsx
+++ src/components/doctor/Doctorh.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Doctorh() {
   return (
     <div className='py-20 mt-10 bg-yellow-800 top-10 text-white font-bold left-0 right-0 px-28 min-h-screen'>
-      <h1>this is Doctor home section,
-        create the Doctor home section here</h1></div>
+      <h1></h1></div>
   )
 }
 
